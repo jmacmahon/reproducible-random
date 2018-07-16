3898278a5257d39e766bfbe450c19c6b9d321b13
Joe MacMahon <joe.macmahon@evergreensmartpower.co.uk>
2018-07-16 09:43:09 +0100

    Changed main export so that we don't regenerate the context every time we call createRandom

--- src/lib.ts
+++ src/lib.ts
@@ -1,7 +1,4 @@
 import { createRandomContext, generateSeedContext, CreateRandom } from './reproducible-random'
 
-const createRandom: CreateRandom = () => {
-  return createRandomContext(generateSeedContext())()
-}
-
+const createRandom: CreateRandom = createRandomContext(generateSeedContext())
 export default createRandom
